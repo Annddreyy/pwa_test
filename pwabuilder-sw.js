44f063b8c73481a5f7957887b7272ea6788cc72b
Andrey22 <andreymail22112006@gmail.com>
2025-03-12 20:18:19 +0300

    Remove onnotification click listener

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -63,8 +63,3 @@ self.addEventListener('push', (event) => {
 		})
 	);
 });
-
-self.addEventListener('notificationclick', (event) => {
-	event.notification.close();
-	event.waitUntil(clients.openWindow('/'));
-});
